8f9203d1015338e53d67ccfe0fb2f5493ecc70f0
LadyMarina <49723271+LadyMarina@users.noreply.github.com>
2020-12-31 17:32:56 +0100

    HUD

---
+++ CursoNavidad-Xnergic/Assets/Codi/HUD.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+    public GameObject player;
+    public Text textVida;
+    public Text textPunts;
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        textVida.text = player.GetComponent<Player>().vida.ToString();
+        textPunts.text = player.GetComponent<Player>().punts.ToString();
+    }
+}
